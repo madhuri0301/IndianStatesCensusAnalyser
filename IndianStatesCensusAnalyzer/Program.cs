9912c7a3707a30b09f0adefafce11d00dd4a274b
Madhuri <harshupatil0301@gmail>
2021-06-29 23:54:51 +0530

    [Madhuri]Add : Welcome Message Uploaded

---
+++ IndianStatesCensusAnalyzer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IndianStatesCensusAnalyzer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To The Indian Census Analyser Problem");
+        }
+    }
+}
